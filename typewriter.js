6154a7ac0e31fdf59ed2e4ad5be87ec747a0933f
rom <mozzieisme@gmail.com>
2021-11-02 10:23:39 -0400

    Refactor file

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,15 @@
 const sentence = "hello there from lighthouse labs";
 
 let timer = 0;
-let interval = 500;
+let interval = 50;
 
-for (const i in sentence) {
+// When running, run it in the terminal! ::
+for (let i = 0; i <= sentence.length; i++) {
   setTimeout(() => {
-    process.stdout.write(sentence[i]);
+    if (sentence[i]) {
+      process.stdout.write(sentence[i]);
+    } else if (i === sentence.length) {
+      console.log("");
+    }
   }, (timer += interval)); // <= 1s delay to make it noticeable. Can dial it down later.
 }
